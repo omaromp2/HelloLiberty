aabfe5b7674f9597c5c960577b29467378a4d599
Omar Ponce <omar.ponce@axcare.com>
2018-10-04 13:44:30 -0400

    randoms

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var config = require('./config.js');
 var T = new Twitter(config);
 var moment = require('moment');
 var date = moment().format('LLL')
-
+// var ran = Math.random().toString(8).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+var random = Math.floor(Math.random() * 1000) + 1;
+var ran = Math.floor(Math.random() * random) + 1;
 
 app.get('/', (req, res) => res.send('Yeah'))
 
@@ -16,7 +18,7 @@ app.get('/tweet', (req, res) => {
 	res.send('Lets tweet');
 
 	var tweet = {
-		status: 'Querido @LibertyPR todavia sin servicio ' + date
+		status: ran + ' - Querido @LibertyPR todavia sin servicio ' + date
 	}
 
 	T.post('statuses/update',tweet, tweetErr);
